53473eaed70ccf0042beaa5853caa6fe5ed617bc
Will Darkins <willdarkins@gmail.com>
2021-10-31 20:50:12 -0700

    comment model and api routes added

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,24 +1,35 @@
 //include packages and models that we'll need to create the Express.js API endpoints.
+const sequelize = require('../../config/connection');
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote, Comment } = require("../../models");
+
 
 // get all posts
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
         /*Notice that the order property is assigned a nested array that orders by the
         created_at column in descending order. This will ensure that the latest posted
         articles will appear first.*/
         order: [['created_at', 'DESC']], 
         //In the next step, we'll include the JOIN to the User table. We do this by adding the property include
         include: [
+            // include the Comment model here:
             {
+              model: Comment,
+              attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+              include: {
                 model: User,
                 attributes: ['username']
+              }
+            },
+            {
+              model: User,
+              attributes: ['username']
             }
-        ]
-    })
+          ]
+         })
         .then(dbPostData => res.json(dbPostData))
         .catch(err => {
             console.log(err);
@@ -34,8 +45,16 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                  model: User,
+                  attributes: ['username']
+                }
+              },
             {
                 model: User,
                 attributes: ['username']
@@ -70,6 +89,37 @@ router.post('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+  //Up vote a post
+  router.put('/upvote', (req, res) => {
+    Vote.create({
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+      }).then(() => {
+        // then find the post we just voted on
+        return Post.findOne({
+          where: {
+            id: req.body.post_id
+          },
+          attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
+            [
+              sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+              'vote_count'
+            ]
+          ]
+        })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+    })
 });
 
 //Update a Post's Title
@@ -119,6 +169,8 @@ router.delete('/:id', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+
 
 //In order to test this route, we need to expose the changes to the router by using the following Express.js command:
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Vote } = require("../../models");
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -25,7 +25,27 @@ router.get('/:id', (req, res) => {
       attributes: { exclude: ['password'] },
       where: {
         id: req.params.id
-      }
+      },
+      include: [
+        {
+          model: Post,
+          attributes: ['id', 'title', 'post_url', 'created_at']
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'created_at'],
+          include: {
+            model: Post,
+            attributes: ['title']
+          }
+        },
+        {
+          model: Post,
+          attributes: ['title'],
+          through: Vote,
+          as: 'voted_posts'
+        }
+      ]
     })
     /*Because we're looking for one user, there's the possibility that we could accidentally
     search for a user with a nonexistent id value. Therefore, if the .then() method returns nothing
